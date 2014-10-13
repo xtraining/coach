bafedbcfcc766534a94e2635b6f1b1c994481ed6
Steven Zhang <hmilvu@163.com>
2014-10-13 15:49:40 +0800

    no message

--- interface/coachproj/src/test/java/com/coach/service/CommonServiceTest.java
+++ interface/coachproj/src/test/java/com/coach/service/CommonServiceTest.java
@@ -14,7 +14,7 @@ import com.coach.utils.HttpUtil;
 import com.rop.utils.RopUtils;
 
 public class CommonServiceTest extends TestCase {
-	public static final String SERVER_URL = "http://114.215.145.26/coach/service";
+	public static final String SERVER_URL = "http://localhost:8080/coach/service";
 	public static final String APP_KEY = "iphone_user";
 	public static final String APP_SECRET = "LlnZA8cql4liN4CvjGL5GfwhCh7fwWGE";
 	public static final String VERSION = "V0.5.0.0";
