0404269e9a94f011f6d8e3ec85ca9adafaeb5ef4
Steven Zhang <hmilvu@163.com>
2014-11-11 15:01:54 +0800

    no message

--- interface/coachproj/src/main/java/com/coach/service/QiniuCallbackService.java
+++ interface/coachproj/src/main/java/com/coach/service/QiniuCallbackService.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import com.coach.request.UploadCallbackRequest;
 import com.coach.response.CallbackSuccessResponse;
+import com.coach.utils.Config;
 import com.rop.annotation.IgnoreSignType;
 import com.rop.annotation.NeedInSessionType;
 import com.rop.annotation.ServiceMethod;
@@ -17,7 +18,7 @@ public class QiniuCallbackService extends SimpleBaseService{
 	@ServiceMethod(method = "qiniu.uploadCallback",version = "1.0",needInSession = NeedInSessionType.NO)
     public Object uploadCallback(UploadCallbackRequest request) { 
 		CallbackSuccessResponse r = new CallbackSuccessResponse();
-		r.setKey(request.getName());
+		r.setKey(Config.getProperty("QINIU_DOMAIN") + request.getName());
 		log.info("callback from qiniu:", request.toString());
 		return r;
 	}
